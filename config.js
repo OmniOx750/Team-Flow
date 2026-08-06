aa90c0701e0a806d9ddc3a6491a156c633975b17
OmniOx750 <mek03@mekics.com>
2026-08-06 16:03:35 +0900

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
  * 팀 접속키는 이 파일에 적지 않습니다. 사용자가 최초 접속 시 직접 입력합니다.
  */
 window.TEAM_FLOW_CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/PASTE_YOUR_DEPLOYMENT_ID_HERE/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbynJfBT5UVoX-hlZbM8YbfrVSPruTlwOdqOFLDGiB90G4Lkh5J9-in01O_pb8WlHLK0/exec',
   REQUEST_TIMEOUT_MS: 20000,
   SYNC_POLL_INTERVAL_MS: 700,
   SYNC_POLL_ATTEMPTS: 7
